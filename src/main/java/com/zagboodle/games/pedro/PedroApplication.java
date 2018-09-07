a3ce5c8c2c6464a3059733094e9b8b545b13b504
Jeremy Jacobs <zagboodle@gmail.com>
2018-09-07 10:43:39 -0500

    Initial project add.

---
+++ src/main/java/com/zagboodle/games/pedro/PedroApplication.java
@@ -0,0 +1,12 @@
+package com.zagboodle.games.pedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedroApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zagboodle/games/pedro/PedroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zagboodle.games.pedro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PedroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
